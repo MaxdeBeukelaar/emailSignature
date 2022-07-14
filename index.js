483dc52e310563ddd0d865d5f33b51d3b7ed8a40
max Beukelaar <maxbeukelaar@maxs-air.home>
2022-07-14 15:50:10 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+function save() {
+
+  document.getElementById("name").innerText =  document.getElementById("inputNaam").value
+  document.getElementById("function").innerText = document.getElementById("inputFunctie").value
+  document.getElementById("email").innerText = document.getElementById("inputEmail").value
+  document.getElementById("tel").innerText = document.getElementById("inputTel").value
+}
